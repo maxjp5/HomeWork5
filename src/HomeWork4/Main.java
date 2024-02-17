04a520d4530f4794b32c1ba311d334f9b31b7b97
Evgeny865 <157610686+Evgeny865@users.noreply.github.com>
2024-02-17 02:45:33 +0200

    HomeWorkNumber 4, secondQ

--- src/HomeWork4/Main.java
+++ src/HomeWork4/Main.java
@@ -1,6 +1,26 @@
-package HomeWork4;public class Main {
+package HomeWork4;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        SemaphoreExample.SharedResource = new SemaphoreExample.SharedResource(2);
+
+        Thread thread1 = new SemaphoreExample.Worker("Thread 1", SemaphoreExample.SharedResource);
+        Thread thread2 = new SemaphoreExample.Worker("Thread 2", SemaphoreExample.SharedResource);
+        Thread thread3 = new SemaphoreExample.Worker("Thread 3", SemaphoreExample.SharedResource);
+
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+            thread3.join();
+
+        } catch (InterruptedException e) {
+        }
     }
 }do you hear me?
 yes mf
