9305a57805055cf8faf1b83cf510219b50227826
maxjp5 <119008848+maxjp5@users.noreply.github.com>
2024-02-25 23:21:22 +0200

    Update Main.java
    added names/id

--- src/HomeWork4/Main.java
+++ src/HomeWork4/Main.java
@@ -1,5 +1,7 @@
 package HomeWork4;
-
+//Evgeny Vasilev 336550686 
+//Maxim Sirotenko 328663547
+//https://github.com/Evgeny865/HomeWork5/tree/master/src/HomeWork4
 public class Main {
     public static void main(String[] args) {
 
